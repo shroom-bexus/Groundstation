10aabf60b7116b0be91e2e04d18c11376bbf7c35
Jacob Zahn <65290566+Sarius23@users.noreply.github.com>
2026-09-22 01:11:01 +0200

    Test enlarged binary telemetry records

--- tests/test_binary_telemetry.py
+++ tests/test_binary_telemetry.py
@@ -15,8 +15,16 @@ class BinaryTelemetryTests(unittest.TestCase):
         self.assertEqual(decode_datagram(b'ACK,5,OK\n'),['ACK,5,OK'])
         self.assertEqual(decode_datagram(b'SEQ,2\nPADS,0,293.15,100000\n'),['SEQ,2','PADS,0,293.15,100000'])
 
+    def test_long_airdos_record(self):
+        line = b'AIRDOS,4294967295,9,$X,' + b'a' * 1000
+        self.assertLessEqual(len(line), 1151)
+        self.assertEqual(
+            decode_datagram(self.packet(b'\0' + line)),
+            ['SEQ,1', line.decode('ascii')]
+        )
+
     def test_corruption(self):
-        for body in [b'',b'\2',b'\1',b'\1\xf0',b'\1\xf1',b'\1\xf1\x05\0ab',b'\1\xf2\xff',b'\0a\nb',b'\0a\0b',b'\1'+b'\xef'*200]:
+        for body in [b'',b'\2',b'\1',b'\1\xf0',b'\1\xf1',b'\1\xf1\x05\0ab',b'\1\xf2\xff',b'\0a\nb',b'\0a\0b',b'\1'+b'\xef'*1200]:
             with self.subTest(body=body), self.assertRaises(ValueError):
                 decode_datagram(self.packet(body))
         for data in [b'SHB2'+b'\0'*10,b'SHB1',self.packet(b'\0okay')+b'\x02']:
